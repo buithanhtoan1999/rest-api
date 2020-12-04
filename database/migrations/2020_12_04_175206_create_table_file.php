92862dfca273843606cd655e9ee26e208d149078
Nguyễn Hiếu <vanhieu13546@gmail.com>
2020-12-05 00:56:13 +0700

    save

--- database/migrations/2020_12_04_175206_create_table_file.php
+++ database/migrations/2020_12_04_175206_create_table_file.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableFiles extends Migration
+class CreateTableFile extends Migration
 {
     /**
      * Run the migrations.
