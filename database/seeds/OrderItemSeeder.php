d87b581b5f8679007983a3aecfa0c92bb9cfceec
buithanhtoan1999 <66494094+buithanhtoan1999@users.noreply.github.com>
2020-12-28 23:21:21 +0700

    update

--- database/seeds/OrderItemSeeder.php
+++ database/seeds/OrderItemSeeder.php
@@ -17,137 +17,137 @@ class OrderItemSeeder extends Seeder
          DB::statement('SET foreign_key_checks = 1');
          $data=[
              [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
+                 'product_id'=>6,
+                 'order_id'=>3,
+                 'count'=>2,
              ],
              [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
+                 'product_id'=>7,
+                 'order_id'=>2,
+                 'count'=>3,
              ],
              [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
+                 'product_id'=>10,
+                 'order_id'=>2,
+                 'count'=>3,
              ],
              [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
+                 'product_id'=>9,
+                 'order_id'=>3,
+                 'count'=>2,
              ],
              [
+                 'product_id'=>12,
+                 'order_id'=>2,
+                 'count'=>3,
+             ], [
+                 'product_id'=>11,
+                 'order_id'=>2,
+                 'count'=>3,
+             ], [
+                 'product_id'=>10,
+                 'order_id'=>3,
+                 'count'=>2,
+             ], [
+                 'product_id'=>10,
+                 'order_id'=>3,
+                 'count'=>2,
+             ], [
+                 'product_id'=>6,
+                 'order_id'=>4,
+                 'count'=>1,
+             ], [
+                 'product_id'=>5,
+                 'order_id'=>3,
+                 'count'=>2,
+             ], [
+                 'product_id'=>3,
+                 'order_id'=>4,
+                 'count'=>2,
+             ], [
+                 'product_id'=>2,
+                 'order_id'=>2,
+                 'count'=>2,
+             ], [
+                 'product_id'=>3,
+                 'order_id'=>2,
+                 'count'=>5,
+             ], [
+                 'product_id'=>4,
+                 'order_id'=>3,
+                 'count'=>2,
+             ], [
+                 'product_id'=>4,
+                 'order_id'=>1,
+                 'count'=>3,
+             ], [
+                 'product_id'=>2,
+                 'order_id'=>3,
+                 'count'=>2,
+             ], [
+                 'product_id'=>3,
+                 'order_id'=>2,
+                 'count'=>5,
+             ], [
                  'product_id'=>1,
+                 'order_id'=>2,
+                 'count'=>4,
+             ], [
+                 'product_id'=>2,
+                 'order_id'=>4,
+                 'count'=>4,
+             ], [
+                 'product_id'=>2,
+                 'order_id'=>3,
+                 'count'=>2,
+             ], [
+                 'product_id'=>4,
+                 'order_id'=>1,
+                 'count'=>3,
+             ], [
+                 'product_id'=>2,
+                 'order_id'=>3,
+                 'count'=>3,
+             ], [
+                 'product_id'=>1,
+                 'order_id'=>3,
+                 'count'=>3,
+             ], [
+                 'product_id'=>1,
+                 'order_id'=>3,
+                 'count'=>4,
+             ], [
+                 'product_id'=>2,
+                 'order_id'=>3,
+                 'count'=>4,
+             ], [
+                 'product_id'=>4,
+                 'order_id'=>2,
+                 'count'=>4,
+             ], [
+                 'product_id'=>3,
+                 'order_id'=>1,
+                 'count'=>3,
+             ], [
+                 'product_id'=>2,
+                 'order_id'=>2,
+                 'count'=>2,
+             ], [
+                 'product_id'=>4,
                  'order_id'=>1,
                  'count'=>1,
              ], [
-                 'product_id'=>1,
+                 'product_id'=>5,
                  'order_id'=>1,
                  'count'=>1,
              ], [
-                 'product_id'=>1,
+                 'product_id'=>4,
                  'order_id'=>1,
-                 'count'=>1,
+                 'count'=>3,
              ], [
-                 'product_id'=>1,
+                 'product_id'=>3,
                  'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
-             ], [
-                 'product_id'=>1,
-                 'order_id'=>1,
-                 'count'=>1,
+                 'count'=>2,
              ],
          ];
         DB::table('orderItems')->insert($data);
